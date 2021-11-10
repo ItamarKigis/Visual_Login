9ca7eeb6940fc8848996167081a49dbeb83ce937
ItamarKigis <93520303+ItamarKigis@users.noreply.github.com>
2021-11-10 13:26:13 +0200

    Add files via upload

---
+++ Navigation Drawer App/Detection.py
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Navigation_Drawer_App
+{
+    class Detection
+    {
+        if __name__ == '__main__':
+            print("DIE")
+    }
+}
